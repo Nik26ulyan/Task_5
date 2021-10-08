9b213103ffa46686607bf537539ae56732ea8ffb
Nik26ulyan <itishnik_nik@mail.ru>
2021-10-08 16:20:46 +0300

    Initial commit

---
+++ src/ru/vsu/sc/uliyanov_n_s/Main.java
@@ -0,0 +1,137 @@
+package ru.vsu.sc.uliyanov_n_s;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printFirstPhrase();
+
+        int s = readSizeOfFigure("Size: ");
+
+        drawFigure(s);
+    }
+
+    static void printFirstPhrase() {
+        System.out.println("Enter the size of the figure with an odd number that is greater than or equal to five.");
+    }
+
+    static int readSizeOfFigure(String text) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(text);
+        int s;
+
+        if (scanner.hasNextInt()) {
+            s = scanner.nextInt();
+
+            if (s < 5 || s % 2 == 0) {
+                System.out.println("Invalid value! Number can't be even adn less than five. Try again.");
+                return readSizeOfFigure(text);
+            }
+
+        } else {
+            System.out.println("Invalid value! You need to enter a INTEGER NUMBER. Try again.");
+            return readSizeOfFigure(text);
+        }
+
+        return s;
+    }
+
+    static void drawFigure(int s) {
+        int tmp = (s - 1) / 2;
+
+        drawBorderLine(s);
+
+        for (int i = 0; i < tmp; i++) {
+            System.out.print("|");
+
+            drawUpperPartOfFigure(i, tmp);
+
+            System.out.print("|");
+            System.out.println();
+        }
+
+        System.out.print("|");
+
+        drawMiddleLineOfFigure(s);
+
+        System.out.print("|");
+        System.out.println();
+
+        for (int i = 0; i < tmp; i++) {
+            System.out.print("|");
+
+            drawLowerPartOfFigure(i, tmp);
+
+            System.out.print("|");
+            System.out.println();
+        }
+
+        drawBorderLine(s);
+    }
+
+    static void drawBorderLine(int s) {
+        System.out.print("+");
+
+        for (int i = 0; i < s; i++) {
+            System.out.print("-");
+        }
+
+        System.out.print("+");
+        System.out.println();
+    }
+
+    static void drawUpperPartOfFigure(int i, int tmp) {
+        for (int j = tmp - i; j < tmp; j++) {
+            System.out.print(" ");
+        }
+
+        System.out.print("*");
+
+        for (int k = tmp - i; k > 1; k--) {
+            System.out.print(" ");
+        }
+
+        System.out.print("*");
+
+        for (int k = tmp - i; k > 1; k--) {
+            System.out.print(" ");
+        }
+
+        System.out.print("*");
+
+        for (int k = tmp - i; k < tmp; k++) {
+            System.out.print(" ");
+        }
+    }
+
+    static void drawMiddleLineOfFigure(int s) {
+        for (int i = 0; i < s; i++) {
+            System.out.print("*");
+        }
+    }
+
+    static void drawLowerPartOfFigure(int i, int tmp) {
+        for (int k = tmp - i; k > 1; k--) {
+            System.out.print(" ");
+        }
+
+        System.out.print("*");
+
+        for (int k = tmp - i; k < tmp; k++) {
+            System.out.print(" ");
+        }
+
+        System.out.print("*");
+
+        for (int j = tmp - i; j < tmp; j++) {
+            System.out.print(" ");
+        }
+
+        System.out.print("*");
+
+        for (int k = tmp - i; k > 1; k--) {
+            System.out.print(" ");
+        }
+    }
+}
